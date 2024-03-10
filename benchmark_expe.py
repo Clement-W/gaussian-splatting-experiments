c7b103bc8737cf30b57968870aa0622605bc3f78
Clément Weinreich <clement@weinreich.fr>
2024-03-10 11:41:49 +0100

    UPdate lr for raindeer

--- benchmark_expe.py
+++ benchmark_expe.py
@@ -96,7 +96,8 @@ def custom_experiments(scenes, args):
 
     for i in range(3):
         # repete experience a few times
-        reg_args = f"--maxvariance_regularization 0.0001 --edge_regularization 0.005 --smoothness_regularization 0.0001"
+        #reg_args = f"--maxvariance_regularization 0.0001 --edge_regularization 0.005 --smoothness_regularization 0.0001"
+        reg_args = f"--maxvariance_regularization 0.00001 --edge_regularization 0.001 --smoothness_regularization 0.00001"
         flag = f"maxvariance_edge_smoothness_{i}"
         run_experiment(reg_args, flag, scenes, args)
 
